207f74385d9bb6eb09a5264aeadb3a6ab1ef503a
OscarGlo <oscarglo@free.fr>
2019-03-23 19:01:09 +0100

    aaaaaaaa

--- app/web/script.js
+++ app/web/script.js
@@ -1,52 +1,52 @@
-String.prototype.capitalize = function () {
+String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.substr(1);
 };
 
 const app = new Vue({
-    el: '#affichage',
+    el: "#affichage",
     data: {
         activites: [],
-        installations: []
+        installations: [],
+        filtres: {},
+        show: {},
+        no_result: false
     },
     methods: {
-        search: async function () {
+        search: async function() {
             let reqParams = [];
-
+            
             let codePostal = this.$refs.codePostal.value;
             if (codePostal !== "") reqParams.push(`codePostal=${codePostal}`);
-    
+            
             let recherche = this.$refs.recherche.value;
             if (recherche !== "") reqParams.push(`q=${recherche}`);
             
             /*let region = this.$refs.region.value;
             if (region !== "" && codePostal === "") reqParams.push(`region=${region}`);*/
-
-            let result = null;
             
             let reqUrl = url + "api/" + (this.$refs.installation.checked ? "installations" : "activites") + "?" + reqParams.join("&");
+            let result = await fetch(reqUrl).then(res => res.json());
             
-            result = await fetch(reqUrl).then(res => res.json());
-            
-            console.log(reqUrl);
-            //console.log(result);
-
-            if (result !== undefined && result !== []) {
-                console.log("result !");
-                console.log(result[0]);
-
-                if(result[0].nomact === undefined){
+            if (result.length > 0) {
+                if (result[0].nomact === undefined) {
                     //console.log(result[0].nomact);
-                    app.installations = result;
-                    app.activites = [];
-                }else {
-                    app.activites = result;
-                    app.installations = [];
+                    this.installations = result;
+                    this.activites = [];
+                } else {
+                    this.activites = result;
+                    this.installations = [];
                 }
-            }else{
-                console.log("nope !");
-                app.activites = [];
-                app.installations = [];
+    
+                this.no_result = false;
+            } else {
+                this.activites = [];
+                this.installations = [];
+                
+                this.no_result = true;
             }
+        },
+        derouler: function(nom) {
+            this.$set(this.show, nom, !this.show[nom]);
         }
     }
 });
